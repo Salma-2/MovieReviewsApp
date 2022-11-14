4a4b768ad52fe63b89e30fa33583e7d37ad25788
= <=>
2022-11-14 21:37:54 +0200

    implemented entity and dao

---
+++ app/src/main/java/com/salma/moviereviewsapp/data/MoviesDatabase.kt
@@ -0,0 +1,14 @@
+package com.salma.moviereviewsapp.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.salma.moviereviewsapp.data.local.MovieDao
+import com.salma.moviereviewsapp.model.Movie
+
+@Database(
+    entities = [Movie::class],
+    version = 1
+)
+abstract class MoviesDatabase : RoomDatabase() {
+    abstract val dao: MovieDao
+}
