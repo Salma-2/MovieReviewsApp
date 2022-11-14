939cb9b2032a671f2b763366c1320fa8a9de8810
= <=>
2022-11-14 22:03:43 +0200

    added modules for network and database

---
+++ app/src/main/java/com/salma/moviereviewsapp/di/NetworkModule.kt
@@ -0,0 +1,57 @@
+package com.salma.moviereviewsapp.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.salma.moviereviewsapp.BuildConfig
+import com.salma.moviereviewsapp.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@ExperimentalSerializationApi
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor {
+                val url = it.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("api-key", BuildConfig.API_KEY)
+                    .build()
+                val requestBuilder = it.request().newBuilder()
+                    .url(url)
+                it.proceed(requestBuilder.build())
+            }
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = MediaType.get("application/json")
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
+
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+    }
+}
--- app/src/main/java/com/salma/moviereviewsapp/util/Constants.kt
+++ app/src/main/java/com/salma/moviereviewsapp/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val BASE_URL = "https://api.nytimes.com/svc/movies/v2/"
     const val REVIEWS_URL = "/reviews/search.json"
     const val MOVIE_TABLE = "movie_table"
+    const val MOVIE_Database = "movie_database"
+
 }
