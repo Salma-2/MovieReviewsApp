81993bb4424698adf38820fdaddd4c5dec257bff
= <=>
2022-11-14 23:38:31 +0200

    provided the repo

---
+++ app/src/main/java/com/salma/moviereviewsapp/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.salma.moviereviewsapp.di
+
+import com.salma.moviereviewsapp.data.MovieRepository
+import com.salma.moviereviewsapp.data.MovieRepositoryImpl
+import com.salma.moviereviewsapp.data.local.MoviesDatabase
+import com.salma.moviereviewsapp.data.remote.NytApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun providesMovieRepository(db: MoviesDatabase, api: NytApi): MovieRepository {
+        return MovieRepositoryImpl(db.dao, api)
+    }
+}
