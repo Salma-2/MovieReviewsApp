8a2b9fc6456ccc168a7ce8e4b6f0d8b80bc3bed6
= <=>
2022-11-14 21:37:48 +0200

    implemented entity and dao

---
+++ app/src/main/java/com/salma/moviereviewsapp/data/local/MovieDao.kt
@@ -0,0 +1,17 @@
+package com.salma.moviereviewsapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.salma.moviereviewsapp.model.Movie
+import com.salma.moviereviewsapp.util.Constants
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface MovieDao {
+    @Query("SELECT * FROM ${Constants.MOVIE_TABLE}")
+    fun getMovies(): Flow<List<Movie>>
+
+    @Query("SELECT * FROM ${Constants.MOVIE_TABLE} WHERE id = :id")
+    suspend fun getMovieById(id: Int): Movie
+}
