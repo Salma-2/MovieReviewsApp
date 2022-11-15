143165f4d1b60bb2d1bb650f90d86b7b8e35a396
= <=>
2022-11-15 16:43:57 +0200

    passed the movie object as a parameter, and added the required dependencies

--- app/src/main/java/com/salma/moviereviewsapp/model/MovieMultimedia.kt
+++ app/src/main/java/com/salma/moviereviewsapp/model/MovieMultimedia.kt
@@ -1,9 +1,13 @@
 package com.salma.moviereviewsapp.model
+
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
+@Parcelize
 @Serializable
 data class MovieMultimedia(
     @SerializedName("src")
-    val imgUrl : String
-)
+    val imgUrl: String,
+) : Parcelable
